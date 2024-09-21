699aa43d4c228522ffde6a18eb525062f76b0504
Alfianzyh <alfianzyh@gmail.com>
2024-09-21 23:54:55 +0700

    landing page update 1

---
+++ landing page/js/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      extend: {
+        animation: {
+          ripple: "ripple var(--duration,2s) ease calc(var(--i, 0)*.2s) infinite",
+        },
+        keyframes: {
+          ripple: {
+            "0%, 100%": {
+              transform: "translate(-50%, -50%) scale(1)",
+            },
+            "50%": {
+              transform: "translate(-50%, -50%) scale(0.9)",
+            },
+          },
+        },
+      },
+    },
+  };
